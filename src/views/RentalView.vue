23e0e67de5e4fee28a24e17518e644c813a760e4
Fred Fonseca <freddcfonseca@gmail.com>
2022-09-15 14:32:06 -0300

    fix: validations on delete #17

--- src/views/RentalView.vue
+++ src/views/RentalView.vue
@@ -447,6 +447,7 @@ export default {
         this.$nextTick(() => {
           this.editedItem = Object.assign({}, this.defaultItem);
           this.editedIndex = -1;
+          this.$refs.form.resetValidation()
         });
       });
     },
